ece81bf8532304072fdacbb0178407e81920b4c4
Sanjay shitole <scshitole@gmail.com>
2024-04-15 13:40:45 -0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -3,16 +3,16 @@ from classes import imagenet_classes
 import grpc
 from ovmsclient import make_grpc_client
 
-def run():
+def run(image_path):
     # Create SSL/TLS channel credentials
     credentials = grpc.ssl_channel_credentials()
 
     # Replace '10.1.1.7:443' with the address of your gRPC server and the SSL port
     # You may need to replace '443' with the actual SSL port of your server
     with grpc.secure_channel('10.1.1.7:443', credentials) as channel:
-        client = make_grpc_client(channel)
+        client = make_grpc_client('10.1.1.7:443')  # Pass the URL as a string here
 
-        with open("zebra.jpeg", "rb") as f:
+        with open(image_path, "rb") as f:
             img = f.read()
 
         # Make a gRPC call
@@ -23,4 +23,9 @@ def run():
     print(imagenet_classes[result_index])
 
 if __name__ == '__main__':
-    run()
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python3 predict.py <image_path>")
+        sys.exit(1)
+    image_path = sys.argv[1]
+    run(image_path)
