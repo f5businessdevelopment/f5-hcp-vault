6021a2519e53a34ced4298c95c754c92fe2e1646
Sanjay shitole <scshitole@gmail.com>
2024-04-15 14:27:55 -0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,23 +1,22 @@
 import numpy as np
 from classes import imagenet_classes
 import grpc
-from ovmsclient import make_grpc_client
 
 def run(image_path):
     # Create SSL/TLS channel credentials
     credentials = grpc.ssl_channel_credentials()
 
-    # Create channel with HTTP/2 support and compression
-    options = [
-        ('grpc.default_compression_algorithm', grpc.Compression.Gzip)
-    ]
-    channel = grpc.secure_channel('10.1.1.7:443', credentials, options)
+    # Create channel with HTTP/2 support and compression options
+    options = [('grpc.default_compression_algorithm', grpc.Compression.Gzip)]
+
+    # Import the make_grpc_client function from ovmsclient
+    from ovmsclient import make_grpc_client
+
+    # Create gRPC client with the server URL and SSL credentials
+    client = make_grpc_client('10.1.1.7:443', ssl_channel_credentials=credentials)
 
     with open(image_path, "rb") as f:
         img = f.read()
-
-    # Make a gRPC client
-    client = make_grpc_client('10.1.1.7:443')
 
     # Make a gRPC call
     output = client.predict({"0": img}, "resnet")
